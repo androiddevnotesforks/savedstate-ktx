eff1ebdc3dc58bdec76e823a690d5eedb177bb9f
Jaewoong Cheon <cheonjaewoong@gmail.com>
2023-01-29 14:49:18 +0900

    Bump AGP from 7.3.1 to 7.4 and gradle 7.4 to 7.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
-        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.android.tools.build:gradle:7.4.0")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.23.1")
     }
 }
